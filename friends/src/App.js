f291426a49dcfb84b4a73203b30b85ac443aa43b
Joscelyn <JoscelynRose@gmail.com>
2019-08-20 13:44:08 -0400

    WIP: setting up privateRoute

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login.js';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute.js';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <Route exact path="/protected" component={GasPrices} />
+      </div>
+    </Router>
   );
 }
 
